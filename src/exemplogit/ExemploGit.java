cd872b3346832c1e24fe7ceb984e96bfef84a882
helderseixas <helderseixas@gmail.com>
2014-11-06 08:14:36 -0200

    A versão inicial do projeto.

---
+++ src/exemplogit/ExemploGit.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package exemplogit;
+
+/**
+ *
+ * @author ALUNO 2014 2
+ */
+public class ExemploGit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Teste git!");
+    }
+    
+}
